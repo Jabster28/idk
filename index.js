40fad4b12540bf8f015eac72a525633f5007d06f
Jabster28 <justynboyer@gmail.com>
2018-07-07 22:10:11 +0100

    Removed some <b> tags i found

--- index.js
+++ index.js
@@ -10,29 +10,13 @@ $(document).ready(function () {
 };
 	if (mobilecheck() == true) {
 	$(".comp").hide()
-	} else {
+} else {
 		$(".mobile").hide()
 	}
 
 const auth = firebase.auth();
-function siweap() {
-	const email = document.getElementById('email').value;
-	const password = document.getElementById('password').value;
-	const txtemail = email
-	const txtpass = password
-	const promise = auth.signInWithEmailAndPassword(email, txtpass);
-	promise.catch(e => console.log(e.message));
+function siweap(a, b,) {
+
+
 }
-
-function suweap() {
-	const email = document.getElementById('email').value;
-	const password = document.getElementById('password').value;
-	const txtemail = email
-	const txtpass = password
-	const promise = auth.createUserWithEmailAndPassword(email, txtpass);
-	promise.catch(e => console.log(e.message));
-}
-
-
-
 })
