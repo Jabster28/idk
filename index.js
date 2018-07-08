61ac239ae820cb4be8f3a24baaee987c7be3ecda
Jabster28 <justynboyer@gmail.com>
2018-07-08 16:54:39 +0100

    Button signs in a user

--- index.js
+++ index.js
@@ -25,9 +25,22 @@ $(document).ready(function() {
     // ...
   });
 
-  $('.btn waves-effect waves-light col s12').on(click, () => {
-    signInWithEmailAndPassword(email, password)
-  })
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+    window.location.replace(/)
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      // ...
+    } else {
+      // User is signed out.
+      // ...
+    }
+  });
 
 
 
