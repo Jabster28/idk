f6a1aa6d95bca1bab00e8c5aa6f252980632d27e
Jabster28 <justynboyer@gmail.com>
2018-07-08 17:20:16 +0100

    I think ive done it

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
   const email = document.getElementById('email').value
   const password = document.getElementById('password').value
 
-  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+  firebase.auth().signInWithEmailAndPassword(email.value, password.value).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
